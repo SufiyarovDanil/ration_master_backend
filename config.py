804a7f176bba81e4ccfbb866b9146fa49b80ee13
Данил Суфияров <danil-sufiyarov@mail.ru>
2024-04-30 18:15:01 +0500

    fix db url name

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 def get_database_url() -> str:
-    return os.environ.get('DATABASEE_URL')
+    return os.environ.get('DATABASE_URL')

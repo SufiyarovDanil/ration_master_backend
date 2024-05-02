1d626813a09dbf58031577e72580fa77b9766de3
Данил Суфияров <danil-sufiyarov@mail.ru>
2024-05-02 18:01:44 +0500

    add exc handler

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi import Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 
 from api.routers.user import router as user_router
+from api.schemas import OutputSchema
+
+
+async def validation_exception_handler(
+        request: Request,
+        exc: RequestValidationError):
+    """
+    Кастомный хэндлер для ошибок валидации входных данных.
+
+    Нужен для соблюдения общего формата ответа.
+
+    """
+    
+    error = exc.errors()[0]
+
+    content: OutputSchema = OutputSchema(
+        data={'arg': error['loc']},
+        error=error["msg"]
+    )
+
+    return JSONResponse(jsonable_encoder(content))
 
 
 app: FastAPI = FastAPI(
@@ -11,3 +37,7 @@ app: FastAPI = FastAPI(
     })
 
 app.include_router(user_router)
+app.add_exception_handler(
+    RequestValidationError,
+    validation_exception_handler
+)

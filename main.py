85a096a6c1fa8cd6656383ae728ef4c201dec977
Данил Суфияров <danil-sufiyarov@mail.ru>
2024-05-07 18:38:07 +0500

    add cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers.product import router as prod_router
 from api.routers.ration import router as ration_router
@@ -35,6 +36,16 @@ async def valid_data_exc_handler(
 app: FastAPI = FastAPI(
     title='Ration master API',
     swagger_ui_parameters={'displayRequestDuration': True}
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(prod_router)

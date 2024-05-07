751f64a00c1fee36ae859a41a31f76e6ab500e66
Данил Суфияров <danil-sufiyarov@mail.ru>
2024-05-07 15:53:58 +0500

    remove user excs

--- api/exceptions.py
+++ api/exceptions.py
@@ -1,13 +1,3 @@
-class UserNotFoundException(Exception):
-    def __init__(self):
-        super().__init__('User not found!')
-
-
-class UserAlreadyExistsException(Exception):
-    def __init__(self):
-        super().__init__('User already exists!')
-
-
 class ProductNotFoundException(Exception):
     def __init__(self):
         super().__init__('Product not found!')

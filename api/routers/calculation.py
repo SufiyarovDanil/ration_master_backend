1bcb8fc604004bf52c60aa8b6ab066a5be7a20de
Данил Суфияров <danil-sufiyarov@mail.ru>
2024-05-10 18:33:45 +0500

    fin

--- api/routers/calculation.py
+++ api/routers/calculation.py
@@ -1,33 +1,17 @@
+import random as rnd
+
 from fastapi import APIRouter
 
 from api.schemas import OutputSchema
 from api.schemas.calculation import GenderEnum, PhysicalActivityEnum
+import src.calculation as calc_service
+import src.product as prod_service
 
 
 router: APIRouter = APIRouter(
     prefix='/api',
     tags=['calculation']
 )
-
-
-def calc_bmr(weight: int, height: int, age: int, gender: GenderEnum) -> float:
-    """
-    Вычисление базового метаболизма
-    """
-    if gender == GenderEnum.male:
-        return 88.36 + 13.4 * weight + 4.8 * height - 5.7 * age
-    
-    return 447.6 + 9.2 * weight + 3.1 * height - 4.3 * age
-
-
-def get_activity_coefficient(physical_activity: PhysicalActivityEnum) -> float:
-    match physical_activity:
-        case PhysicalActivityEnum.weight_gain:
-            return 1.9
-        case PhysicalActivityEnum.weight_keep:
-            return 1.55
-        case PhysicalActivityEnum.weight_loss:
-            return 1.2
 
 
 @router.get('/calculate_cpfc/{age}/{weight}/{height}/{gender}/{physical_activity}')
@@ -38,32 +22,41 @@ async def calculate_cpfc(
     Вычисление калорий и БЖУ
     """
 
-    bmr: float = calc_bmr(weight, height, age, gender)
-    ac: float = get_activity_coefficient(physical_activity)
-    calorie: float = bmr * ac
-
-    if physical_activity == PhysicalActivityEnum.weight_loss:
-        calorie -= 750.0
-    if physical_activity == PhysicalActivityEnum.weight_gain:
-        calorie += 400.0
-    
-    tdee: float = 0.0
-
-    if gender == GenderEnum.male:
-        tdee = 655.0 + 9.6 * weight + 1.8 * height - 4.7 * age
-    else:
-        tdee = 66.0 + 13.7 * weight + 5.0 * height - 6.8 * age
-
-    protein: float = weight * 1.9
-    fat: float = (tdee * 0.275) / 9.0
-    carbohydrate: float = (tdee - protein * 4.0 - fat * 9.0) / 4.0
+    cpfc = calc_service.calc_cpfc(age, weight, height, gender, physical_activity)
     result = OutputSchema()
-
-    result.data = {
-        "calorie": calorie,
-        "protein": protein,
-        "fat": fat,
-        "carbohydrate": carbohydrate
-    }
+    result.data = cpfc
 
     return result
+
+
+@router.get('/calculate_ration/{age}/{weight}/{height}/{gender}/{physical_activity}')
+async def calculate_ration(
+    age: int, weight: int, height: int, gender: GenderEnum,
+    physical_activity: PhysicalActivityEnum) -> OutputSchema:
+    """
+
+    """
+    cpfc = calc_service.calc_cpfc(age, weight, height, gender,
+                                  physical_activity)
+    prods = prod_service.get_all_products()
+    calorie_per_meal = cpfc['calorie'] / 3.0
+    fitted_products = [{
+        'name': i.name,
+        'calorie': calorie_per_meal,
+        'protein': i.protein * (calorie_per_meal / i.calorie),
+        'fat': i.fat * (calorie_per_meal / i.calorie),
+        'carbohydrate': i.carbohydrate * (calorie_per_meal / i.calorie),
+        'gramms': 100.0 * (calorie_per_meal / i.calorie)
+    } for i in prods]
+    ration = {
+        'breakfast': None,
+        'dinner': None,
+        'supper': None
+    }
+
+    for meal in ration.keys():
+        product = rnd.choice(fitted_products)
+        ration[meal] = product
+        fitted_products.remove(product)
+
+    return OutputSchema(data={'cpfc': cpfc, 'ration': ration})
---
+++ src/calculation.py
@@ -0,0 +1,55 @@
+from api.schemas.calculation import GenderEnum, PhysicalActivityEnum
+
+
+def __calc_bmr(weight: int, height: int, age: int, gender: GenderEnum) -> float:
+    """
+    Вычисление базового метаболизма
+    """
+    if gender == GenderEnum.male:
+        return 88.36 + 13.4 * weight + 4.8 * height - 5.7 * age
+    
+    return 447.6 + 9.2 * weight + 3.1 * height - 4.3 * age
+
+
+def __get_activity_coefficient(physical_activity: PhysicalActivityEnum) -> float:
+    match physical_activity:
+        case PhysicalActivityEnum.weight_gain:
+            return 1.9
+        case PhysicalActivityEnum.weight_keep:
+            return 1.55
+        case PhysicalActivityEnum.weight_loss:
+            return 1.2
+
+
+def calc_cpfc(age: int, weight: int, height: int, gender: GenderEnum,
+         physical_activity: PhysicalActivityEnum) -> dict[str, int]:
+    """
+    Вычисление калорий и БЖУ
+    """
+
+    bmr: float = __calc_bmr(weight, height, age, gender)
+    ac: float = __get_activity_coefficient(physical_activity)
+    calorie: float = bmr * ac
+
+    if physical_activity == PhysicalActivityEnum.weight_loss:
+        calorie -= 750.0
+    if physical_activity == PhysicalActivityEnum.weight_gain:
+        calorie += 400.0
+    
+    tdee: float = 0.0
+
+    if gender == GenderEnum.male:
+        tdee = 655.0 + 9.6 * weight + 1.8 * height - 4.7 * age
+    else:
+        tdee = 66.0 + 13.7 * weight + 5.0 * height - 6.8 * age
+
+    protein: float = weight * 1.9
+    fat: float = (tdee * 0.275) / 9.0
+    carbohydrate: float = (tdee - protein * 4.0 - fat * 9.0) / 4.0
+
+    return {
+        "calorie": calorie,
+        "protein": protein,
+        "fat": fat,
+        "carbohydrate": carbohydrate
+    }
